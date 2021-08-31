7a7d14802ed052f09d878994053e62a3710e981d
namancg <naman.c@perfios.com>
2021-09-01 01:36:08 +0530

    REFACTORED: other queue operations included

--- src/com/bridgelabz/stacksandqueues/Queue.java
+++ src/com/bridgelabz/stacksandqueues/Queue.java
@@ -2,12 +2,26 @@ package com.bridgelabz.stacksandqueues;
 import com.bridgelabz.linkedlists.*;
 public class Queue <K>{
 private final LinkedListImpl queueLinkedList;
-	
 	public Queue() {
 		queueLinkedList = new LinkedListImpl();
 	}
 	public void enqueue(INode element) {
 		queueLinkedList.append(element);
+	}
+	public INode dequeue() {
+		return queueLinkedList.delete();
+	}
+	public int size() {
+		return queueLinkedList.size();
+	}
+	public boolean isEmpty() {
+		if(queueLinkedList.head != null) {
+			return false;
+		}
+		else {
+			return true;
+		}
+		
 	}
 	public void printQueue() {
 		queueLinkedList.printLinkedList();
